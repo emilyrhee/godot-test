204c82118e56670fa584debecae55a119b469004
cvoges12 <38054771+cvoges12@users.noreply.github.com>
2024-01-20 12:35:45 -0600

    Fixed inconsistent indentation
    There were a lot of 4 space indentations and tab characters. So I just replaced the tab characters with 4 spaces to make it more consistent for improved readability on other editors / IDEs

--- player/Player.cs
+++ player/Player.cs
@@ -2,50 +2,50 @@ using Godot;
 using System;
 
 public partial class Player : CharacterBody2D {
-	private int speed = 100;
+    private int speed = 100;
     private int health = 30;
-	private AnimationPlayer animation;
+    private AnimationPlayer animation;
 
-	public override void _Ready() {
-		animation = GetNode<AnimationPlayer>("AnimationPlayer");
-	}
-
-	private void UpdateAnimation() {		
-		if (Velocity.Length() == 0) {
-			animation.Stop();
-		} else {
-			string direction = "down";
-
-		if (Velocity.X != 0) {
-			if (Velocity.X < 0) {
-				direction = "left";
-			} else {
-				direction = "right";
-			}
-		} else {
-			if (Velocity.Y < 0) {
-				direction = "up";
-			} else {
-				direction = "down";
-			}
-		}
-
-			animation.Play("walk_" + direction);
-		}
-	}
-
-    private void Move() {
-		Vector2 inputVector = new Vector2(
-			Input.GetActionStrength("d") - Input.GetActionStrength("a"),
-			Input.GetActionStrength("s") - Input.GetActionStrength("w")
-		).Normalized();
-
-		Velocity = inputVector * speed;
-		MoveAndSlide();
+    public override void _Ready() {
+        animation = GetNode<AnimationPlayer>("AnimationPlayer");
     }
 
-	public override void _Process(double delta) {
+    private void UpdateAnimation() {		
+        if (Velocity.Length() == 0) {
+            animation.Stop();
+        } else {
+            string direction = "down";
+
+            if (Velocity.X != 0) {
+                if (Velocity.X < 0) {
+                    direction = "left";
+                } else {
+                    direction = "right";
+                }
+            } else {
+                if (Velocity.Y < 0) {
+                    direction = "up";
+                } else {
+                    direction = "down";
+                }
+            }
+
+            animation.Play("walk_" + direction);
+        }
+    }
+
+    private void Move() {
+        Vector2 inputVector = new Vector2(
+            Input.GetActionStrength("d") - Input.GetActionStrength("a"),
+            Input.GetActionStrength("s") - Input.GetActionStrength("w")
+        ).Normalized();
+
+        Velocity = inputVector * speed;
+        MoveAndSlide();
+    }
+
+    public override void _Process(double delta) {
         Move();
-		UpdateAnimation();
-	}
+        UpdateAnimation();
+    }
 }
