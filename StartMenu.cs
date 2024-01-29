daa74e2ce175ca8182358da2f892cf4dcc25e781
emilyrhee <emilyrhee99@gmail.com>
2024-01-28 22:21:57 -0500

    start menu responds to keyboard input

--- StartMenu.cs
+++ StartMenu.cs
@@ -2,11 +2,18 @@ using Godot;
 using System;
 
 public partial class StartMenu : Node2D {
-	private void _on_play_pressed() {
-		GetTree().ChangeSceneToFile("res://world.tscn");
-	}
+    private Button startMenuButtons;
+    public override void _Ready() {
+        startMenuButtons = GetNode<Button>("StartMenuButtons/Play");
 
-	private void _on_quit_pressed() {
-		GetTree().Quit();
-	}
+        startMenuButtons.GrabFocus();
+    }
+
+    private void _on_play_pressed() {
+        GetTree().ChangeSceneToFile("res://world.tscn");
+    }
+
+    private void _on_quit_pressed() {
+        GetTree().Quit();
+    }
 }
