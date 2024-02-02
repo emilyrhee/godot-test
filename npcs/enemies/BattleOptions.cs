8314fed37aa981b55d516c664a2f121ee4281941
emilyrhee <emilyrhee99@gmail.com>
2024-02-01 21:26:55 -0500

    made battle buttons pressable

---
+++ npcs/enemies/BattleOptions.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class BattleOptions : VBoxContainer {
+    private TextureButton fight;
+	public override void _Ready() {
+        fight = GetNode<TextureButton>("Fight");
+
+        fight.GrabFocus();
+	}
+
+    private void _on_fight_pressed() {
+        GD.Print("hi");
+    }
+}
